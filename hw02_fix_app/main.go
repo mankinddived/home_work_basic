c4edc8dbc7680a30c54285bd015953be35d6edbe
mankinddived <mankinddived@gmail.com>
2024-01-11 13:59:59 +0800

    hw02_fix_app_change1

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,13 +1,14 @@
-package init
+package main
 
 import (
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
 	"fmt"
+
+	"github.com/mankinddived/home_work_basic/hw02_fix_app/printer"
+	"github.com/mankinddived/home_work_basic/hw02_fix_app/reader"
+	"github.com/mankinddived/home_work_basic/hw02_fix_app/types"
 )
 
-func init() {
+func main() {
 	var path string = "data.json"
 
 	fmt.Printf("Enter data file path: ")
@@ -18,7 +19,6 @@ func init() {
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
 	staff, err = reader.ReadJSON(path, -1)
